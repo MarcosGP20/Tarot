d84590a33a54b706a107af4c79a43cbf69aac2d5
MarcosGP20 <110936978+MarcosGP20@users.noreply.github.com>
2024-02-11 18:06:34 -0300

    Add files via upload

---
+++ Prueba/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const navList = document.querySelector(".nav-list");
+
+  mobileMenu.addEventListener("click", function () {
+    navList.classList.toggle("active");
+  });
+
+  const contactForm = document.getElementById("contact-form");
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    alert("Mensaje enviado correctamente");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const whatsappBtn = document.getElementById("whatsapp-btn");
+  whatsappBtn.addEventListener("click", function () {
+    const phoneNumber = "543517962346";
+    const message = "Hola, quiero una consulta gratuita.";
+
+    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      message
+    )}`;
+
+    window.open(whatsappLink, "_blank");
+  });
+});
